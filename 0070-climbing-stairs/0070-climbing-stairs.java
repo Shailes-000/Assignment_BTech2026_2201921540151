8172e91fba1f15fb809e394d0e00b3803b84d2c2
Shailes-000 <tiwarishailesh612@gmail.com>
2025-02-07 17:47:49 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int one = 1;
+        int two = 1;
+        for (int i = 2; i <= n; ++i) {
+            int temp = one + two;
+            one = two;
+            two = temp;
+        }
+        return two;
+    }
+}
